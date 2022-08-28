1e7b0e4471d08492db8d3cbb53f0415ab8232ef0
Usman <bkayani50@gmail.com>
2022-08-28 11:57:42 +0500

    card fix

--- components/Mint.js
+++ components/Mint.js
@@ -317,6 +317,8 @@ export function Mint() {
 										<li>
 											<a
 												// href={`https://testnets.opensea.io/collection/${config.addressOrName}`}
+												// href={`https://testnets.opensea.io/collection/${config.addressOrName}`}
+												// href={`https://testnets.opensea.io/collection/${config.addressOrName}`}
 												href="https://testnets.opensea.io/account?search[resultModel]=ASSETS&search[sortBy]=CREATED_DATE&search[sortAscending]=false"
 												target="_blank"
 												rel="noreferrer"
